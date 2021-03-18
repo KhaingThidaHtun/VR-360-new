6a30a76832f36da7167b69078216fb7f7f99f91c
KhaingThidaHtun <khaingthidahtun@ad.gicjp.com>
2021-03-18 12:04:56 +0900

    first commit

---
+++ app-files/data.js
@@ -0,0 +1,48 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-11walls",
+      "name": "11.walls",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        }
+      ],
+      "faceSize": 381,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.31813170622418774,
+          "pitch": 0.16317315648690034,
+          "rotation": 0,
+          "target": "0-11walls"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.391881850128188,
+          "pitch": -0.16452686267972183,
+          "title": "Title",
+          "text": "Hello"
+        }
+      ]
+    }
+  ],
+  "name": "Sample New",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
